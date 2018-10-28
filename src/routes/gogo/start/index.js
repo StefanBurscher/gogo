52ade190a7e2f5706328317d689117f4774f1ed7
StefanBurscher <stefan.burscher@gmail.com>
2018-10-28 21:47:14 +0100

    ...

--- src/routes/gogo/start/index.js
+++ src/routes/gogo/start/index.js
@@ -40,14 +40,14 @@ class Start extends Component {
     let getAccessTokenUrl = "";
     switch (props.socialNetwork) {
       case "INSTAGRAM":
-        url = "https://www.instagram.com/accounts/login/?next=/oauth/authorize/%3Fclient_id%3D5df48e0684bc4e349f2f093cd9cf953c%26redirect_uri%3Dhttp%3A//207.180.216.94/api/v1/users/register_access_token/%26response_type%3Dtoken"
+        getAccessTokenUrl = "https://www.instagram.com/accounts/login/?next=/oauth/authorize/%3Fclient_id%3D5df48e0684bc4e349f2f093cd9cf953c%26redirect_uri%3Dhttp%3A//207.180.216.94/api/v1/users/register_access_token/%26response_type%3Dtoken"
         break;
 
       default:
         break;
     }
     return (
-      <Button outline color="primary" className="mb-2" onClick={() => this.openLink(url)}>
+      <Button outline color="primary" className="mb-2" onClick={() => this.openLink(getAccessTokenUrl)}>
         <IntlMessages id={"button." + (connected ? "manage" : "connect")} />
       </Button>
     )
