6ca7db59c962bd1bcb08c3e60598360c54bfa1d8
StefanBurscher <stefan.burscher@gmail.com>
2018-10-30 16:11:02 +0100

    ...

--- src/redux/auth/reducer.js
+++ src/redux/auth/reducer.js
@@ -16,19 +16,13 @@ export default (state = INIT_STATE, action) => {
   switch (action.type) {
     case LOGIN_USER:
       return { ...state, loading: true };
+    case LOGIN_USER_SUCCESS:
+      // notify.success('Login Success');
+      return { ...state, loading: false };
     case REGISTER_USER:
       return { ...state, loading: true };
     case REGISTER_USER_SUCCESS:
-      // notify.success('Register User Success');
-      NotificationManager.success(
-        "Register User Success",
-        "Title here",
-        3000,
-        null,
-        null,
-        ''
-      );
-      return { ...state, loading: false, user: action.payload.id };
+      return { ...state, loading: false };
     case LOGOUT_USER:
       return { ...state, user: null };
     default:
